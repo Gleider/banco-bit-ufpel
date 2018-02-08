c9a4102b9c5aeebc6cf31bc1e2975b369ca599a7
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-02-08 13:35:09 -0200

    Organizando os comentários

--- cliente/interfaceLogin.py
+++ cliente/interfaceLogin.py
@@ -2,36 +2,43 @@ from interfaceConfig import *
 
 login = ['0']
 
+# classe para o login
 class Login(object):
     def __init__(self, janela, usuarios):
         self.usuarios = usuarios
         self.janela = janela
 
-        self.lbt = Label(janela, text='Tela de Login', bg=corFundo, fg=corLetra, font=("Verdana", 24))   #Define o label dentro da janela dizendo a mensagem do topo
-        self.lbt.place(x=300, y=50)              #Adiciona o label na posição especificada
+        # armazena o texto para colocar no label referente ao texto superior
+        self.lbt = Label(janela, text='Tela de Login', bg=corFundo, fg=corLetra, font=("Verdana", 24))
+        self.lbt.place(x=300, y=50)
 
-        self.entLogin = Entry(janela, width='18', font=('verdana', 18))        #cria a caixa de texto de login
-        self.entLogin.place(x=300, y=200)     #posiciona a caixa de login
+        # define o campo de texto referente ao número da conta
+        self.entLogin = Entry(janela, width='18', font=('verdana', 18))
+        self.entLogin.place(x=300, y=200)
 
-        self.entSenha = Entry(janela, show="*", width='18', font=('verdana', 18))        #cria a caixa de texto da senha
-        self.entSenha.place(x=300, y=300)     #posiciona a caixa de texto da senha
+        # define o campo de texto referente a senha
+        self.entSenha = Entry(janela, show="*", width='18', font=('verdana', 18))
+        self.entSenha.place(x=300, y=300)
 
-        self.lbLogin = Label(janela, text="Login", bg=corFundo, fg=corLetra, font=('verdana', 20))     #cria label dizendo login
-        self.lbLogin.place(x=200, y=200)      #posiciona label login
+        # define a label com o texto Login
+        self.lbLogin = Label(janela, text="Login", bg=corFundo, fg=corLetra, font=('verdana', 20))
+        self.lbLogin.place(x=200, y=200)
 
-        self.lbSenha = Label(janela, text="Senha", bg=corFundo, fg=corLetra, font=('verdana', 20))     #cria label dizendo senha
-        self.lbSenha.place(x=200, y=300)      #posiciona label senha
+        # define a label com o texto senha
+        self.lbSenha = Label(janela, text="Senha", bg=corFundo, fg=corLetra, font=('verdana', 20))
+        self.lbSenha.place(x=200, y=300)
 
+        # define o botão entrar
         self.btEntrar = Button(janela, text="Entrar", bg="#115", fg="white", width='8', height='3',
                                font=('verdana', 20), command=self.entrar)
         self.btEntrar.place(x=350, y=400)
 
+        # define o botão sair
         self.btSair = Button(janela, text="Sair", bg="#115", fg="white", width='10', height='3', command=self.exit)
         self.btSair.place(x=50, y=450)
 
-
+    # função para solicitar acesso de usuário no servidor
     def entrar(self):
-        # log e senha serão verificados no banco de dados
         logSucc = False
         log = str(self.entLogin.get())
         senha = str(self.entSenha.get())
@@ -44,10 +51,12 @@ class Login(object):
         if not logSucc:
             messagebox.showinfo('Informação', 'Não foi possível realizar login')
 
+    # função para encerrar a aplicação
     def exit(self):
         messagebox.showinfo('Informação', 'Saindo...')
         exit()
 
+# quando finalizar a loop da interface, retornará opção definida
 def main(usuarios):
     inicio(Login, usuarios)
     return login[0]
