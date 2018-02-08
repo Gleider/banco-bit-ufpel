edfb5033dfca8f4952654abab7a310c2b32f2777
Gleider <gmdcampos@inf.ufpel.edu.br>
2018-02-08 21:42:27 -0200

    mudando o estilo de retorno para tratar melhorar nas outras interfaces

--- cliente/interfaceLogin.py
+++ cliente/interfaceLogin.py
@@ -1,30 +1,13 @@
 from interfaceConfig import *
 
-from cryptography.fernet import Fernet
-import base64, pickle
-from socket import *
-
-#chave criptografica
-
-try:
-    serverName='192.168.0.2'
-    serverPort= 1024
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName, serverPort))
-
-except:
-    messagebox.showinfo('Informação', 'Sistema fora do ar')
-    exit()
-
-
-login = [None]
+login = []
 
 # classe para o login
 class Login(object):
     def __init__(self, janela, usuarios):
         self.usuarios = usuarios
         self.janela = janela
-        self.key = base64.urlsafe_b64encode(b'BANCOBITUFPEL0123456789ABCDEFGHI') 
+       
 
         # armazena o texto para colocar no label referente ao texto superior
         self.lbt = Label(janela, text='Tela de Login', bg=corFundo, fg=corLetra, font=("Verdana", 24))
@@ -58,17 +41,18 @@ class Login(object):
     # função para solicitar acesso de usuário no servidor
     def entrar(self):
         # pega as informações inseridas nos campos de texto
-        log = int(str(self.entLogin.get()))
-        senha = str.encode(self.entSenha.get())
+        numConta = int(str(self.entLogin.get()))
+        senhaCripto = str.encode(self.entSenha.get())
+        setNumConta(numConta)
     
         # vetor contendo valores enviados ao servidor [operacao, numContaRem, numContaDest, valor, senha]
-        vetor = [0, log, None, None, senha]
+        vetor = [0, numConta, None, None, senhaCripto]
     
         # transforma objeto em sequência de byte
         s = pickle.dumps(vetor)
         
         # criptografa a mensagem
-        s = self.criptografar(s) 
+        s = criptografar(s) 
 
         # envia a mensagem criptografada
         clientSocket.sendall(s) 
@@ -77,7 +61,7 @@ class Login(object):
         msg = clientSocket.recv(2048)
 
         # decriptogrando mensagem
-        msg = self.decriptografar(msg) 
+        msg = decriptografar(msg) 
 
         #transforma a sequência de byte em objeto
         msg = pickle.loads(msg)
@@ -85,32 +69,28 @@ class Login(object):
         # caso o retorno do servidor seja 0, então o login foi executado com sucesso
         if(msg[0] == 0):
             messagebox.showinfo('Informação', 'Login executado com sucesso')
-            login[0] = msg
+
+            # adiciona o retorno do servidor [0] e o número da conta [1]
+            login.append(msg)
+            login.append(vetor[1])
+            
             self.janela.destroy()
 
         # caso o retorno do servidor seja 1, então ocorreu um erro ao tentar acessar a conta
         else:
             messagebox.showinfo('Informação', 'Erro ao tentar acessar a conta')
-        print(msg)
     
     # função para encerrar a aplicação
     def exit(self):
         messagebox.showinfo('Informação', 'Saindo...')
         exit()
 
-    # funções para critografar e decriptografar as mensagens do servidor
-    def criptografar(self, conteudo):
-        result = Fernet(self.key)
-        return result.encrypt(conteudo)
-
-    def decriptografar(self, conteudo):
-        result = Fernet(self.key)
-        return result.decrypt(conteudo)
+    
 
 # quando o login for feito com sucesso, irá retornar o vetor contendo as informações da conta
 def main(usuarios):
     inicio(Login, usuarios)
-    return login[0]
+    return login
 
 
 
